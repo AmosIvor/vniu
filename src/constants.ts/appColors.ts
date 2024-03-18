2e57793194f9584b920452963dfc591015e82faa
AmosIvor <trantuanvu00000@gmail.com>
2024-03-18 15:41:58 +0700

    [COMPONENT] add component text and row

---
+++ src/constants.ts/appColors.ts
@@ -0,0 +1,7 @@
+export const appColors = {
+  primary: '#1A94FF',
+  text: '#1D1E20',
+  text2: '#8F959E',
+  bgPrimary: '#FEFEFE',
+  gray: '#F5F6FA'
+}
---
+++ src/screens/products/ProductSearchScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+
+const ProductSearchScreen = () => {
+  return (
+    <View>
+      <Text>ProductSearchScreen</Text>
+    </View>
+  )
+}
+export default ProductSearchScreen
