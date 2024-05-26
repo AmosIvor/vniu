836a5982bd0d002bdb1c3497ce2d1ba74eba0967
AmosIvor <trantuanvu00000@gmail.com>
2024-05-26 13:41:47 +0700

    [CHAT] add connection chathub to implement real-time chat

--- src/apis/chat.api.ts
+++ src/apis/chat.api.ts
@@ -1,4 +1,4 @@
-import { ChatRoomResponseType } from '@appTypes/chat.type'
+import { ChatRoomResponseType, MessageRequestType, MessageResponseType } from '@appTypes/chat.type'
 import { SuccessResponse } from '@appTypes/utils.type'
 import { apis } from '@constants'
 
@@ -7,6 +7,14 @@ import { http } from '@utils'
 const chatApi = {
   getChatRoomByUser(userId: string) {
     return http.get<SuccessResponse<ChatRoomResponseType>>(`api/Chat/chatrooms/${userId}`)
+  },
+
+  getMessagesByUser(userId: string) {
+    return http.get<SuccessResponse<MessageResponseType[]>>(`api/Chat/chatrooms/${userId}/messages`)
+  },
+
+  sendMessageByUser({ userId, body }: { userId: string; body: MessageRequestType }) {
+    return http.post<SuccessResponse<MessageResponseType>>(`api/Chat/chatrooms/${userId}/messages`, body)
   }
 }
 
--- src/types/chat.type.ts
+++ src/types/chat.type.ts
@@ -1,4 +1,22 @@
 export interface ChatRoomResponseType {
-  chatRoomId: string
+  chatRoomId: number
   userId: string
+}
+
+export interface MessageResponseType {
+  messageId: number
+  messageContent: string
+  imageUrl: string
+  isFromUser: boolean
+  messageCreateAt: string
+  messageReadAt: string
+  isRead: boolean
+  chatRoomId: number
+}
+
+export interface MessageRequestType {
+  messageContent: string
+  imageUrl?: string
+  isFromUser: boolean
+  isRead: boolean
 }
